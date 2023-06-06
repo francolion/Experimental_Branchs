dfcc4d524f0f46fe36f9f757f6db4fbc71e02e2c
Franco <francofran1998@gmail.com>
2023-06-06 16:24:20 -0300

    Segundo cambio

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React bro
+          aprendiendo a mergear papu!
         </a>
       </header>
     </div>
