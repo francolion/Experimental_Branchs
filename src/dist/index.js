1d4193b96c208dc7fbcbe3bd712ec323080b37cb
Valkiria5 <dharmavaninaramayoquilinan@gmail.com>
2023-06-07 12:24:38 -0300

    cambios en typescript

---
+++ src/dist/index.js
@@ -0,0 +1,35 @@
+"use strict";
+var champiñon = 'honguito con queso';
+champiñon = 'pizza';
+champiñon = 'anana';
+let extincionDinosaurios = 76000000;
+let dinosaurioFavorito = 'T-Rex';
+let extintos = true;
+let mivariable;
+mivariable = 56;
+mivariable = 'gato malo';
+function chanchitofeliz(config) {
+    return config;
+}
+let animales = ['chanchito', 'feliz', 'felipe'];
+let nums = [1, 2, 3, 4];
+let checks = [];
+let nums2 = [];
+let tupla = [1, 'chanchito feliz', 4, true];
+const extrachica = 'xs';
+const chica = 's';
+const mediana = 'm';
+const grande = 'l';
+const extragrande = 'xl';
+var Talla;
+(function (Talla) {
+    Talla["ExtraChica"] = "xs";
+    Talla["Chica"] = "s";
+    Talla["Mediana"] = "m";
+    Talla["Grande"] = "l";
+    Talla["ExtraGrande"] = "xl";
+})(Talla || (Talla = {}));
+const variable1 = Talla.Grande;
+console.log(variable1);
+const estado = 2;
+//# sourceMappingURL=index.js.map
--- src/index.js
+++ src/index.js
@@ -1,18 +1,55 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+/*
+* tipos de JS
+* number
+* string
+*boolean
+* null
+* undefined
+* object
+* function
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-  //este es un comentario chupame la pija 
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+* tipos de TS
+* any >>>>>> intentar no usar. Nos deja poner el valor que nosotros quisieramos y poder cambiarlo, es contraproducente porque al pedo usamos typescript
+* unknown
+* never
+* arrays
+* tuplas
+* Enums
+
+
+* tipos inferidos
+*/
+var extincionDinosaurios = 76000000;
+var dinosaurioFavorito = 'T-Rex';
+var extintos = true;
+var mivariable;
+mivariable = 56;
+mivariable = 'gato malo';
+function chanchitofeliz(config) {
+    return config;
+}
+var animales = ['chanchito', 'feliz', 'felipe'];
+var nums = [1, 2, 3, 4];
+var checks = [];
+var nums2 = [];
+//animales.map(x => x.)// el autocompletado sugiere metodos segun el valor de la variable
+var tupla = [1, 'chanchito feliz', 4, true];
+var extrachica = 'xs';
+var chica = 's';
+var mediana = 'm';
+var grande = 'l';
+var extragrande = 'xl';
+//pascalCase 
+var Talla;
+(function (Talla) {
+    Talla["ExtraChica"] = "xs";
+    Talla["Chica"] = "s";
+    Talla["Mediana"] = "m";
+    Talla["Grande"] = "l";
+    Talla["ExtraGrande"] = "xl";
+})(Talla || (Talla = {}));
+var variable1 = Talla.Grande;
+console.log(variable1);
+var estado = 2 /* LoadingState.Succes */;
+var objeto = { id: 1, nombre: 'pedro', talla: Talla.Chica };
